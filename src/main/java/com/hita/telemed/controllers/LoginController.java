1a8ae5be0f6b939da5e4002dc0eaad25580c3b2d
drc <dadopavlekovic@gmail.com>
2025-01-14 09:16:19 +0100

    testirati obavezno

--- src/main/java/com/hita/telemed/controllers/LoginController.java
+++ src/main/java/com/hita/telemed/controllers/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/telemed")
+@Controller
 public class LoginController {
 
     private AppUserRepository appUserRepository;
